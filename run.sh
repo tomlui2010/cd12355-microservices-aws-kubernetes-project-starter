b7fbcfcd3ecbedf21aa34bbf78380bf38d7b3ce6
THOMAS LOUIS <tomlui2010@gmail.com>
2024-08-23 19:21:57 +1000

    added screenshots required for resubmission

---
+++ run.sh
@@ -0,0 +1,6 @@
+  kubectl apply -f deployment/configmap.yaml 
+  kubectl apply -f deployment/postgresql-deployment.yaml 
+  kubectl apply -f deployment/postgresql-service.yaml 
+  kubectl apply -f deployment/pvc.yaml 
+  kubectl apply -f deployment/pv.yaml 
+  kubectl apply -f deployment/coworking.yaml 
